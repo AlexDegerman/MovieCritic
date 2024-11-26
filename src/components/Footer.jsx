36a813c5cfe2f705dec1e1a7cdce14bc0bfd9d89
AlexDegerman <alex.degerman.dev@gmail.com>
2024-11-27 00:14:42 +0200

    Edit email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; <a href="mailto:moviecritic@gmail.com">moviecritic@gmail.com</a></p>
+        <p>&copy; <a href="mailto:moviecriticweb@gmail.com">moviecriticweb@gmail.com</a></p>
         <ul>
           <li><a href="https://www.linkedin.com/in/alex-degerman-402907245/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
           <li><a href="https://github.com/AlexDegerman" target="_blank" rel="noopener noreferrer">GitHub</a></li>
